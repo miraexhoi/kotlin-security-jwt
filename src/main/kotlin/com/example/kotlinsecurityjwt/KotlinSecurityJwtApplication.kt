c062d774d4413851e171f00187381d09d0f6abea
최미래 <miraechoi@dgsw.hs.kr>
2024-03-21 15:19:33 +0900

    Initial commit

---
+++ src/main/kotlin/com/example/kotlinsecurityjwt/KotlinSecurityJwtApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinsecurityjwt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSecurityJwtApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSecurityJwtApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinsecurityjwt/KotlinSecurityJwtApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinsecurityjwt
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSecurityJwtApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
