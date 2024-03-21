f997601f76313c598ea3b48bc0e547de202f55a0
최미래 <miraechoi@dgsw.hs.kr>
2024-03-21 23:42:34 +0900

    Feat :: enum class 체크용 annotation 생성

---
+++ src/main/kotlin/com/example/kotlinsecurityjwt/common/annotation/ValidEnum.kt
@@ -0,0 +1,36 @@
+package com.example.kotlinsecurityjwt.common.annotation
+
+import jakarta.validation.Constraint
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import jakarta.validation.Payload
+import kotlin.reflect.KClass
+
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
+@Constraint(validatedBy = [ValidEnumValidator::class])
+annotation class ValidEnum(
+    val message: String = "Invalid enum value",
+    val groups: Array<KClass<*>> = [],
+    val payload: Array<KClass<out Payload>> = [],
+    val enumClass: KClass<out Enum<*>>
+)
+
+/**
+ * enumclass 를 체크하는 클래스
+ */
+class ValidEnumValidator: ConstraintValidator<ValidEnum, Any> {
+    private lateinit var enumValues: Array<out Enum<*>>
+
+    override fun initialize(annotation: ValidEnum) {
+        enumValues = annotation.enumClass.java.enumConstants
+    }
+
+    override fun isValid(value: Any?, context: ConstraintValidatorContext): Boolean {
+        if (value == null) {
+            return true
+        }
+        return enumValues.any { it.name == value.toString() }
+    }
+}
