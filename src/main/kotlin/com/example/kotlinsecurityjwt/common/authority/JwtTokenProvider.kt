bb58fe7571d0f8e85405b6853e3854a200bfd4db
최미래 <miraechoi@dgsw.hs.kr>
2024-03-27 23:07:01 +0900

    Feat :: token 정보 추출 함수 생성

--- src/main/kotlin/com/example/kotlinsecurityjwt/common/authority/JwtTokenProvider.kt
+++ src/main/kotlin/com/example/kotlinsecurityjwt/common/authority/JwtTokenProvider.kt
@@ -1,14 +1,19 @@
 package com.example.kotlinsecurityjwt.common.authority
 
+import io.jsonwebtoken.Claims
 import io.jsonwebtoken.Jwts
 import io.jsonwebtoken.SignatureAlgorithm
 import io.jsonwebtoken.io.Decoders
 import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.Authentication
 import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Component
-import java.util.Date
+import java.util.*
 
 const val EXPIRATION_MILLISECONDS: Long = 1000 * 60 * 30
 
@@ -39,4 +44,27 @@ class JwtTokenProvider {
 
         return TokenInfo("Bearer", accessToken)
     }
+
+    /** token 정보 추출 */
+    fun getAuthentication(token: String): Authentication {
+        val claims: Claims = getClaims(token)
+
+        val auth = claims["auth"] ?: throw RuntimeException("잘못된 토큰입니다.")
+
+        /* 권한 정보 추출 */
+        val authorities = (auth as String)
+            .split(",")
+            .map {SimpleGrantedAuthority(it)}
+
+        val principal: UserDetails = User(claims.subject,"",authorities)
+
+        return UsernamePasswordAuthenticationToken(principal,"",authorities)
+    }
+
+    private fun getClaims(token: String): Claims =
+        Jwts.parserBuilder()
+            .setSigningKey(key)
+            .build()
+            .parseClaimsJws(token)
+            .body
 }
