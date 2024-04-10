6c3d2000a59f7ea94a31c3c5cb4bbb2ec73b98da
최미래 <miraechoi@dgsw.hs.kr>
2024-04-10 23:57:53 +0900

    Feat :: 회원가입 시 권한 저장용 repository 생성

--- src/main/kotlin/com/example/kotlinsecurityjwt/member/repository/MemberRepository.kt
+++ src/main/kotlin/com/example/kotlinsecurityjwt/member/repository/MemberRepository.kt
@@ -1,8 +1,11 @@
 package com.example.kotlinsecurityjwt.member.repository
 
 import com.example.kotlinsecurityjwt.member.entity.MemberEntity
+import com.example.kotlinsecurityjwt.member.entity.MemberRole
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MemberRepository: JpaRepository<MemberEntity, Long> {
     fun findByLoginId(loginId: String): MemberEntity?
 }
+
+interface MemberRoleRepository : JpaRepository<MemberRole, Long>
