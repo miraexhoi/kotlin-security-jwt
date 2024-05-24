fa4151affc91dd78a7e918d86b7b8b6af3d099e8
최미래 <miraechoi@dgsw.hs.kr>
2024-05-24 12:40:27 +0900

    Fix :: gradle error 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,12 +9,15 @@ plugins {
     kotlin("plugin.jpa") version kotlinVersion
 }
 
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+    jvmToolchain(17)
+}
+
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-
-java {
-    sourceCompatibility = JavaVersion.VERSION_17
-}
 
 repositories {
     mavenCentral()
@@ -33,6 +36,7 @@ dependencies {
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation(kotlin("stdlib-jdk8"))
 }
 
 allOpen {
@@ -49,7 +53,6 @@ tasks.withType<KotlinCompile> {
         jvmTarget = "17"
     }
 }
-
 tasks.withType<Test> {
     useJUnitPlatform()
 }
