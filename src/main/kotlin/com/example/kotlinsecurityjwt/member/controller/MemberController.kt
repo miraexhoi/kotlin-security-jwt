f7f945f2871170815394eb04417ef6a9e518cb81
최미래 <miraechoi@dgsw.hs.kr>
2024-07-25 20:56:52 +0900

    feat :: create login api

--- src/main/kotlin/com/example/kotlinsecurityjwt/member/controller/MemberController.kt
+++ src/main/kotlin/com/example/kotlinsecurityjwt/member/controller/MemberController.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinsecurityjwt.member.controller
 
+import com.example.kotlinsecurityjwt.common.authority.TokenInfo
 import com.example.kotlinsecurityjwt.common.dto.BaseResponse
+import com.example.kotlinsecurityjwt.member.dto.LoginDto
 import com.example.kotlinsecurityjwt.member.dto.MemberDtoRequest
 import com.example.kotlinsecurityjwt.member.service.MemberService
 import jakarta.validation.Valid
@@ -18,5 +20,12 @@ class MemberController (
     fun signUp(@RequestBody @Valid memberDtoRequest: MemberDtoRequest): BaseResponse<Unit> {
         val resultMsg: String = memberService.signUp(memberDtoRequest)
         return BaseResponse(message = resultMsg)
+    }
+
+    /** 로그인 */
+    @RequestMapping("/login")
+    fun login(@RequestBody @Valid loginDto: LoginDto): BaseResponse<TokenInfo> {
+        val tokenInfo = memberService.login(loginDto)
+        return BaseResponse(data = tokenInfo)
     }
 }
