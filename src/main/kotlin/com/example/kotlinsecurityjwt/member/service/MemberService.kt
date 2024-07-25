225175e3f85c82c49836a4f7f5c07216b4aaaa4b
최미래 <miraechoi@dgsw.hs.kr>
2024-07-25 20:55:02 +0900

    feat :: login service logic
    로그인 시 토큰 발행

--- src/main/kotlin/com/example/kotlinsecurityjwt/member/service/MemberService.kt
+++ src/main/kotlin/com/example/kotlinsecurityjwt/member/service/MemberService.kt
@@ -1,13 +1,19 @@
 package com.example.kotlinsecurityjwt.member.service
 
+import com.example.kotlinsecurityjwt.common.authority.JwtTokenProvider
+import com.example.kotlinsecurityjwt.common.authority.TokenInfo
 import com.example.kotlinsecurityjwt.common.exception.InvalidInputException
 import com.example.kotlinsecurityjwt.common.status.ROLE
+import com.example.kotlinsecurityjwt.member.dto.LoginDto
 import com.example.kotlinsecurityjwt.member.dto.MemberDtoRequest
 import com.example.kotlinsecurityjwt.member.entity.MemberEntity
 import com.example.kotlinsecurityjwt.member.entity.MemberRole
 import com.example.kotlinsecurityjwt.member.repository.MemberRepository
 import com.example.kotlinsecurityjwt.member.repository.MemberRoleRepository
 import jakarta.transaction.Transactional
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 
 
@@ -15,7 +21,9 @@ import org.springframework.stereotype.Service
 @Service
 class MemberService (
     private val memberRepository: MemberRepository,
-    private val memberRoleRepository: MemberRoleRepository
+    private val memberRoleRepository: MemberRoleRepository,
+    private val authenticationManagerBuilder: AuthenticationManagerBuilder,
+    private val jwtTokenProvider: JwtTokenProvider
 ) {
     /** 회원가입 */
     fun signUp(memberDtoRequest: MemberDtoRequest): String {
@@ -32,5 +40,13 @@ class MemberService (
         memberRoleRepository.save(memberRole)
 
         return "회원가입이 완료 되었습니다."
+    }
+
+    /** 로그인 -> 토큰 발행 */
+    fun login(loginDto: LoginDto): TokenInfo {
+        val authenticationToken = UsernamePasswordAuthenticationToken(loginDto.loginId, loginDto.password)
+        val authentication = authenticationManagerBuilder.`object`.authenticate(authenticationToken)
+
+        return jwtTokenProvider.createToken(authentication)
     }
 }
